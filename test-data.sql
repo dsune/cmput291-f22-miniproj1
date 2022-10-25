872cf77ce227372067bebec83267ed25458182f6
Dan Sune <dansune@Dans-MacBook-Pro.local>
2022-10-24 18:42:34 -0600

    initial files

---
+++ test-data.sql
@@ -0,0 +1,12 @@
+-- Data prepapred by Dan Sune, sune@ualberta.ca
+
+PRAGMA foreign_keys = ON;
+
+-- USERS
+INSERT INTO `users` (`name`,`uid`,`pwd`)
+VALUES
+  ("Kaseem Pitts",3,"tellus,"),
+  ("Kareem Short",8,"Suspendisse"),
+  ("Conan Salazar",5,"non"),
+  ("Keith Fuentes",6,"convallis"),
+  ("Lilah Guerrero",8,"lacus.");
